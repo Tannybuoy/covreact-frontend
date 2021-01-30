d4b88bcc0f787d1b0e457d73326ec4106d0e1ab7
redxzeta <xzetanzero@gmail.com>
2021-01-30 11:01:58 -0800

    landing styling

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,20 @@
+import { Button } from "react-bootstrap";
 import FullPageContainer from "../../components/layout/FullPageContainer";
 import "./landing.css";
-const title = "Covax-19 is the hubspot for Covid vaccine reviews"
-const subtitle="You can update us with your post-vaccine symptons, read other reviews and explore our FAQ"
+
 const LandingPage = () => {
   return (
     <FullPageContainer>
-      <div className="landing--panels panel-green"></div>
-      <div className="landing--panels panel-yeet">
+      <div className="landing--panels__left"></div>
+      <div className="landing--panels__right">
+        <h1>Covax-19 is the hubspot for Covid vaccine reviews</h1>
+        <p>
+          You can update us with your post-vaccine symptons, read other reviews
+          and explore our FAQ
+        </p>
+        <button className="landing-button-blue">Write A Review</button>
+        <button className="landing-button-blue">Write A Review</button>
+        <button className="landing-button-blue">Write A Review</button>
       </div>
     </FullPageContainer>
   );
